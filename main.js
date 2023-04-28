62dc38d9bbe7f7b995b6b27dcdba547afc10259f
Gianluca Starke <48135682+XerlockHomeless@users.noreply.github.com>
2023-04-28 11:09:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
+    data(){
+        return{
+            product: "Socks",
+            image: "./assets/images/socks_green.jpg",
+            // image: "./assets/images/socks_blue.jpg",
+            description: "Meia verde",
+            url: "https://www.youtube.com/watch?v=xgunYHNmMLM"
         }
     }
 })
